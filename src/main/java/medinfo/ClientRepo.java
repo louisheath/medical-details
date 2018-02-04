4d9fbc0e48d2db5e4c6ed42a9ae7ab9b68deb1e6
Louis Heath <lh16421@bristol.ac.uk>
2018-02-04 00:51:11 +0000

    restart

---
+++ src/main/java/medinfo/ClientRepo.java
@@ -0,0 +1,10 @@
+package medinfo;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepo extends CrudRepository<Client, String> {
+
+}
